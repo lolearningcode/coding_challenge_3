45630e5955ef5c78e5ebad85b96d4338ebafb87c
lolearningcode <lolearningcode@gmail.com>
2025-05-09 10:33:31 -0700

    use linux image for ami and added gitignore

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# Pull the latest Ubuntu AMI from Canonical
-data "aws_ami" "ubuntu" {
+# Pull the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
@@ -12,9 +12,8 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["137112412989"] # Amazon
 }
-
 # Generate unique suffix for bucket
 resource "random_id" "bucket_id" {
   byte_length = 4
@@ -110,7 +109,7 @@ resource "aws_security_group" "web_sg" {
 }
 # EC2 Instance
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   key_name                    = "newpair" 
   subnet_id                   = aws_subnet.public.id
